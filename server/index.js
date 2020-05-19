452c5b820e4339a0de47a8a6a928df3580c5d011
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-05-19 14:00:51 -0600

    'Initial Commit'

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const ctrl = require('./controller')
+const SERVER_PORT = 4000
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+  console.log("We got a request cap'n")
+  next()
+})
+
+app.get('/api/andrew', (req, res) => {
+  console.log('I GOT A REQUEST A /api/andrew')
+  res.status(200).send('Andrew')
+})
+
+app.get('/api/users', ctrl.getAllUsers)
+app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
+
+app.listen(SERVER_PORT, () =>
+  console.log(`Server running on port ${SERVER_PORT}`)
+)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      users: [],
+      first_name: '',
+      last_name: '',
+      email: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/users').then((res) => {
+      this.setState({
+        users: res.data,
+      })
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = () => {
+    const { first_name, last_name, email } = this.state
+
+    axios.post('/api/users', { first_name, last_name, email }).then((res) => {
+      this.setState({
+        users: res.data,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Andrew's list of people</h1>
+        <input
+          name="first_name"
+          onChange={(e) => this.handleChange(e)}
+          placeholder="First"
+        />
+        <input
+          name="last_name"
+          onChange={(e) => this.handleChange(e)}
+          placeholder="Last"
+        />
+        <input
+          name="email"
+          onChange={(e) => this.handleChange(e)}
+          placeholder="Email"
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+        {this.state.users.map((e) => (
+          <p>
+            {e.first_name} {e.last_name}
+          </p>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
